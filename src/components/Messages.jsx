95a4d5518409e4b5416e3ea70564bab3c27c576d
Tomasz Sędor <itsme@localhost.localdomain>
2020-09-08 06:56:20 +0200

    template for messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const MessagesList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+const MessagesListItem = styled.li`
+  background-color: #ffffff;
+  border: 1px #efefef solid;
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+`;
 
 const Messages = () => {
-  return <>Messages</>
+  return (
+    <MessagesList>
+      <MessagesListItem>
+      </MessagesListItem>
+    </MessagesList>
+  )
 }
 
 export default Messages;
